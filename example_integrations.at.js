ce7c9c92200cb7f81cd151a5569aae3ba97dac93
lailys <laily@alan.app>
2021-11-09 11:23:33 -0800

    Updat the format

--- example_integrations.at.js
+++ example_integrations.at.js
@@ -45,14 +45,20 @@
 // - Electron: https://alan.app/docs/client-api/web/electron
 // - Web Component: https://alan.app/docs/client-api/web/web-component
 
-question('what is this', p =>  {
-    p.play('This is an example app with voice capabilities. (Powered by Alan|Voice support is provided by Alan)');
+question("what is this", "what dies this app do?", (p) => {
+  p.play(
+    "This is an example app with voice capabilities. (Powered by Alan|Voice support is provided by Alan)"
+  );
 });
 
-question('what is Alan (AI|Platform|)', p => {
-    p.play('Alan (AI|Platform) is a platform that will allow you to voice enable any application. Be it mobile app on iOS or Android, or a web page.');
+question("what is Alan (AI|Platform|)", (p) => {
+  p.play(
+    "Alan (AI|Platform) is a platform that will allow you to voice enable any application. Be it mobile app on iOS or Android, or a web page."
+  );
 });
 
-question('what (platforms|SDK|frameworks) are supported', p => {
-    p.play('Alan (AI|Platform) supports native iOS, Android, and Web applications. As well as Flutter, Ionic and many other popular frameworks.');
+question("what (platforms|SDK|frameworks) are supported", (p) => {
+  p.play(
+    "Alan (AI|Platform) supports native iOS, Android, and Web applications. As well as Flutter, Ionic and many other popular frameworks."
+  );
 });
