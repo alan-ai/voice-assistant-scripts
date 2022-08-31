c610e908207662a08edaeefd7f885a15fb494f1f
Anton Ermilin <etioff@gmail.com>
2022-08-31 17:35:58 +0300

    patterns

--- Food_Ordering/Food_Ordering_5_QnA.js
+++ Food_Ordering/Food_Ordering_5_QnA.js
@@ -12,35 +12,29 @@ corpus(
 );
 
 projectAPI.greet = (p, param, callback) => {
-    p.play("Welcome to the Food Ordering demo app for food delivery. (How can I help you|What can I get for you|May I take your order|What would you like to order)?");
+    p.play("Welcome to the food ordering demo app! (How can I help you|What can I get for you|May I take your order|What would you like to order)?");
 };
 
-intent(
-    "What is (it|the app|application)",
-    "Where am I",
-    p => {
-        p.play("(This is|It's) (just|simple) Food Ordering example application for (food delivery service|pizza ordering).");
-    }
-);
+intent("What is (it|the app|application)", p => {
+    p.play("This is an example application to (order food|order pizza and street food) and get a delivery");
+});
 
 intent(
     "What (kind|types) of food do you have (to order|)",
     "What do you have (to order|)",
     "What (food|) is available",
     "What can I (order|have|get)",
+    "(What is|What's) on the menu",
     p => {
-        p.play(
-            "We have several pizzas, street foods, desserts, and drinks available. (What would you like to order?|)",
-            "We offer pizzas, street foods, desserts, and drinks. (What would you like to order?|)"
-        );
+        p.play("We (have several|offer) pizzas, street foods, desserts, and drinks available. (What would you like to order?|To place an order, choose a food category and add items from the menu to the cart.)");
     }
 );
 
 intent(
-    "What (can|should|must) I (do|say|pronounce)",
+    "What (can|should|must) I (do|say|pronounce) (here|)",
     "Help (me|)",
+    "Where am I",
     "What to do (here|)",
-    "How to start",
     p => {
         let route = p.visual.route ? p.visual.route.toLowerCase() : null;
         switch (route) {
@@ -48,27 +42,27 @@ intent(
             case "/menu/street food":
             case "/menu/dessert":
             case "/menu/drink":
-                p.play("Here you can navigate through the menu and add and remove food to your order. To open menu category say (Open|go to) (drinks|pizza|street food|desserts). To add an item to your cart say 'add taco or add (2|3) (burgers|margaritas|latte)'. To remove an item from your cart say 'remove taco or remove (2|3) (burgers|margritas)'. To finish order and checkout say that is all or checkout.");
+                p.play("Here you can navigate through the menu and add or remove food from your order. To open a menu category, say '(Open|go to|show me) (drinks|pizza|street food|desserts)'. To add an item to your cart, say 'add taco' or 'add (2|3) (burgers|margaritas|latte)'. To remove an item from your cart, say 'remove taco' or 'remove (2|3) (burgers|margritas)'. To finish order and checkout, say 'that is all' or 'checkout'.");
                 break;
             case "/cart":
-                p.play("You are in your cart. You should answer questions about delivery address and time. You can change the address by saying 'set address' and you can change delivery time when you say 'set time'.");
+                p.play("You are in your cart. Here you will answer a few questions to provide the delivery details. You can change the address by saying 'set address' and update the delivery time with the 'set time' voice command.");
                 break;
                 //TODO: Not returned in VS route
             case "time":
-                p.play("Enter or say what time we should deliver an order to you.");
+                p.play("Please, enter or say at what time we should deliver your order.");
                 break;
                 //TODO: Not returned in VS route
             case "address":
                 p.play(
-                    "Here you can point the address for delivering your order.",
-                    "Please, enter or say what is the delivery address?"
+                    "Here you can provide the address to deliver your order.",
+                    "Please, enter or say the delivery address"
                 );
                 break;
             case "/finish-order":
-                p.play("You finished your order, if you want to make another order say 'go back' or 'open menu' and add new items to your order.");
+                p.play("Your order is complete. To place another order, say 'go back' or 'open menu' and add items to the cart");
                 break;
             case "/cleared-order":
-                p.play("You have (cleared|canceled) your order, if you want to make another order say 'go back' or 'open menu' and add new items to your order.");
+                p.play("(Your order is canceled|Your cart is empty now). To place another order, say 'go back' or 'open menu' and add items to the cart.");
                 break;
             default:
                 p.play(
@@ -80,9 +74,10 @@ intent(
 );
 
 intent(
-    "How to (make an|) order",
-    "Give me an (order|) example",
+    "(Can you|Please|) (give me|provide) (an|) (order|) example",
+    "How (to|do I) (start|begin) (the|) (dialog|conversation|)",
+    "How (should I|can I|do I|to) (place an order|make an order|order) (food|something|)",
     p => {
-        p.play("Choose food category and add items from menu to order. For example, you can say: (select pizza, add 3 pepperoni, checkout|open street food, add 5 burgers, if you wish to remove some items say remove one burger, what is my order? checkout|open drinks, add one latte, add one cappuccino, that is all)");
+        p.play("Choose a food category and (add|select) items from the menu. For example, you can say: (select pizza, add 3 pepperoni, checkout|open street food, add 5 burgers. to remove some items, say remove: one burger. To check your order, say: what is in the cart? Once ready, say: checkout|open drinks, add one latte, add one cappuccino, that is all)");
     }
 );
